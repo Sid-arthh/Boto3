afac8513314bc796867c1ea588589035ce0ad151
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-07-11 17:33:03 +0530

    Update CheckS3LifeCycleEnable.py

--- CheckS3LifeCycleEnable.py
+++ CheckS3LifeCycleEnable.py
@@ -27,3 +27,39 @@ def check_s3_lifecycle_policies():
                 print(f"Bucket: {bucket_name} does not have any lifecycle policies.")
         except s3_client.exceptions.NoSuchLifecycleConfiguration:
             print(f"Bucket: {bucket_name} does not have any lifecycle policies.")
+
+
+import boto3
+import botocore
+
+def lambda_handler(event, context):
+    check_s3_lifecycle_policies()
+    
+def check_s3_lifecycle_policies():
+    # Create an S3 client
+    s3_client = boto3.client('s3')
+
+    # Get the list of all S3 buckets
+    response = s3_client.list_buckets()
+
+    # Iterate through each bucket
+    for bucket in response['Buckets']:
+        bucket_name = bucket['Name']
+
+        # Get the lifecycle configuration for the bucket
+        try:
+            response = s3_client.get_bucket_lifecycle_configuration(Bucket=bucket_name)
+            lifecycle_configuration = response['Rules']
+
+            # Check if the bucket has any lifecycle policies
+            if lifecycle_configuration:
+                print(f"Bucket: {bucket_name} has lifecycle policies.")
+            else:
+                print(f"Bucket: {bucket_name} does not have any lifecycle policies.")
+        except botocore.exceptions.NoCredentialsError:
+            print(f"Bucket: {bucket_name} does not have any lifecycle policies.")
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == 'NoSuchLifecycleConfiguration':
+                print(f"Bucket: {bucket_name} does not have any lifecycle policies.")
+            else:
+                print(f"Error retrieving lifecycle policies for bucket: {bucket_name}.")
